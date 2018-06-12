84cc9146e84e80d5e0ffe1dfa9dbd61a9bea145b
antonella <antonella_caroca@hotmail.com>
2018-06-12 02:50:32 -0400

    agregoejercicio numero 15 de arrays

--- areValidCredentials.js
+++ areValidCredentials.js
@@ -7,4 +7,5 @@ function areValidCredentials(name, password) {
     }else{
       return false;
     }
+    
   }
---
+++ calculateBillTotal.js
@@ -0,0 +1,10 @@
+function calculateBillTotal(preTaxAndTipAmount) {
+    // your code here
+    preTaxAndTipAmount= 0.245;
+    let total;
+    let result = (total* preTaxAndTipAmount)+total;
+    return result;
+  }
+  
+  module.exports = calculateBillTotal;
+  
