845527e281814197ad9bf042c8e9844f3c409bac
antonella <antonella_caroca@hotmail.com>
2018-06-12 01:42:23 -0400

    agrego 4° ejercicio de retos de codigo de arrays

---
+++ areValidCredentials.js
@@ -0,0 +1,10 @@
+function areValidCredentials(name, password) {
+ 
+    if(name.length >2 && password.length >=8){
+      areValidCredentials ===true;
+      return true;
+  
+    }else{
+      return false;
+    }
+  }
